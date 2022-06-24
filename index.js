1b563caec05fea89cb6bc5df61fe2f53776a7561
Gamerx20003 <you@example.com>
2022-06-24 11:43:57 +0530

    wnd commit

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
 const express = require('express');
+const app = express();
+const path = require('path');
+
 const https = require('https');
 const bodyParser = require('body-parser');
 
-const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
+app.use("/public", express.static(path.resolve(__dirname, 'public')));
 
 app.get("/", function(req,res){
    res.sendFile(__dirname + "/index.html");
-
-
 });
-
-app.post("/weather", function(req,res){
-    const city = req.body.city; 
+app.get("/search.html", function(req,res){
+  res.sendFile(__dirname + "/search.html");
+});
+app.post("/weather", function(requ,resp){
+    const city = requ.body.city; 
+    console.log(city);
     const url="https://api.openweathermap.org/data/2.5/weather?q="+ city +"&appid=6a4227723f6e8913004ac97c3ab2e032";
     https.get(url, function(response){
         response.on("data",function(data){
         const weatherdata = JSON.parse(data);
         const temp= weatherdata.main.temp;
-        res.write("The temp at "+ city +" is " + temp +" kelvin");
-        res.send();       
-      });
-    });
-});
+        resp.write("The temp at "+ city +" is " + temp +" kelvin");
+        resp.send();       
+      })
+    })
+})
 
 app.listen(process.env.PORT || 3000);
